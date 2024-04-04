82ba6e307e1894147cb6a1cd0ab43e1a90d9e72e
Haswanth01101 <147820442+Haswanth01101@users.noreply.github.com>
2024-04-04 11:41:20 +0530

    array.cpp

--- Task1/src/array.cpp
+++ Task1/src/array.cpp
@@ -1,117 +1,114 @@
-#include "array.h"
+	#include "array.h"
 
-array::array(int sz):size(sz),count(0),sum(0){
-	arr=new int[size];
-}
-void array::insert(int data)
-{
-	if(count<size)
-	{
-		arr[count]=data;
-		sum+=data;
-		count++;
-	}else{
-		cout<<"Array is full"<<endl;
-	}	
-}
-
-void array::sumOfEntireArray()
-{
-	cout<<"Sum of the Array Elemets :"<<sum<<endl;
-}
-int array::MaxValue()
-{
-	int Max=arr[0];
-	for(int i=0;i<count;i++)
-	{
-		if(Max<arr[i])
-		{
-			Max=arr[i];
-		}
+	array::array(int sz):size(sz),count(0),sum(0){
+		arr=new int[size];
 	}
-	return Max;
-
-}
-int array::MinValue()
-{
-	int Min=arr[0];
-	for(int i=0;i<count;i++)
+	void array::insert(int data)
 	{
-		if(Min>arr[i])
+		if(count<size)
 		{
-			Min=arr[i];
-		}
+			arr[count]=data;
+			sum+=data;
+			count++;
+		}else{
+			cout<<"Array is full"<<endl;
+		}	
 	}
-	return Min;
-}
+
+	void array::sumOfEntireArray()
+	{
+		cout<<"Sum of the Array Elemets :"<<sum<<endl;
+	}
+	int array::MaxValue()
+	{
+		int Max=arr[0];
+		for(int i=0;i<count;i++)
+		{
+			if(Max<arr[i])
+			{
+				Max=arr[i];
+			}
+		}
+		return Max;
+
+	}
+	int array::MinValue()
+	{
+		int Min=arr[0];
+		for(int i=0;i<count;i++)
+		{
+			if(Min>arr[i])
+			{
+				Min=arr[i];
+			}
+		}
+		return Min;
+	}
 void array::average()
-{
-	double avg=(double)sum/(double)count;
-	cout<<"Average Number is :"<<avg<<endl;
-}
-void array::search(int value)
-{
-	for(int i=0;i<count;i++)
 	{
-	  if(value==arr[i])
-	  {
-	 cout<<"The value found at index : "<<i<<endl;
-	 break;
-	  }
+		double avg=(double)sum/(double)count;
+		cout<<"Average Number is :"<<avg<<endl;
 	}
-	cout<<"The value not found in the array"<<endl;
-}
-void array::sort()
-{
-for(int i=0;i<count;i++)
-{
-int minindex=i;
-for(int j=i+1;j<count;j++)
-{
-	if(arr[j]<arr[minindex])
+	void array::search(int value)
 	{
-		minindex=j;
-	}
-}
-swap(arr[i],arr[minindex]);
-}
-
-cout<<"The sorted Order of Numbers: ";
-for(int i=0;i<count;i++){
-	cout<<arr[i]<<"  ";
-}
-cout<<endl;
-}
-void array::EvenNumbers()
-{
-	cout<<"This are the Following Even Numbers: ";
-	for(int i=0;i<count;i++)
-	{
-		if(arr[i]%2==0)
+		for(int i=0;i<count;i++)
 		{
-		cout<<arr[i]<<"\t";
+		if(value==arr[i])
+		{
+		cout<<"The value found at index : "<<i<<endl;
+		break;
 		}
+		}
+		cout<<"The value not found in the array"<<endl;
+	}
+	void array::sort()
+	{
+	    int i,j;
+        for(i=0;i<size-1;i++){
+            for(j=0;j<size-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int t=arr[j];
+                        arr[j]=arr[j+1];
+                        arr[j+1]=t;
+                    }
+                }
+	    }
+	cout<<"The sorted Order of Numbers: ";
+	for(int i=0;i<count;i++){
+		cout<<arr[i]<<"  ";
 	}
 	cout<<endl;
-}
-void array::OddNumbers()
-{ 
-	for(int i=0;i<count;i++)
-	{
-		if(arr[i]%2!=0)
-		{
-		cout<<arr[i]<<"\t";
-		}
-	}cout<<endl;
-}
-void array::display(){
-	for(int i=0;i<count;i++)
-	{
-		cout<<arr[i]<<"\t";
-
 	}
-	cout<<endl;
-}
+	void array::EvenNumbers()
+	{
+		cout<<"This are the Following Even Numbers: ";
+		for(int i=0;i<count;i++)
+		{
+			if(arr[i]%2==0)
+			{
+			cout<<arr[i]<<"\t";
+			}
+		}
+		cout<<endl;
+	}
+	void array::OddNumbers()
+	{ 
+		for(int i=0;i<count;i++)
+		{
+			if(arr[i]%2!=0)
+			{
+			cout<<arr[i]<<"\t";
+			}
+		}cout<<endl;
+	}
+	void array::display(){
+		for(int i=0;i<count;i++)
+		{
+			cout<<arr[i]<<"\t";
+
+		}
+		cout<<endl;
+	}
 
 
 
