f3ba9939efddfee69538bc9d778a1c5b5e6ae168
Haswanth01101 <147820442+Haswanth01101@users.noreply.github.com>
2024-04-16 11:16:52 +0530

    Update main.cpp

--- Task4/Test/main.cpp
+++ Task4/Test/main.cpp
@@ -1,10 +1,33 @@
-#include "jsonfile.h"
+#include <iostream>
+#include <jsonapp/jsonfile.h>
 int main() {
     string jsonFilePath;
     cout << "Enter the path to the JSON file: ";
     cin >> jsonFilePath;
     JsonApp app(jsonFilePath);
-    app.searchById();
-    app.displayApplications();
+    bool continueExecution = true;
+    while (continueExecution) {
+        cout << "Choose an option:"<<endl;
+        cout << "1. Search by ID" << endl;
+        cout << "2. Display applications" <<endl;
+        cout << "3. Exit" <<endl;
+        int option;
+        cin >> option;
+        switch (option) {
+            case 1:
+                app.searchById();
+                break;
+            case 2:
+                app.displayApplications();
+                break;
+            case 3:
+                continueExecution = false;
+                cout << "Exiting..." << std::endl;
+                break;
+            default:
+                cout << "Invalid option. Please choose again." <<endl;
+                break;
+        }
+    }
     return 0;
 }
