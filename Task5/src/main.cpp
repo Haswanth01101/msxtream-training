207ee2af79e0e33105ff49b8b4a9a19c992cdc27
Haswanth01101 <147820442+Haswanth01101@users.noreply.github.com>
2024-04-23 15:27:50 +0530

    main.cpp

--- Task5/src/main.cpp
+++ Task5/src/main.cpp
@@ -1,27 +1,26 @@
 #include "http_json.h"
 #include <iostream>
 using namespace std;
-int main() {
-    string jsonpath;
-    cout << "Enter Source JSON File path: ";
-    cin >> jsonpath;
-    string configpath = "./config/configuration.json";
-    HttpJson* server = new HttpJson(jsonpath, configpath);
+int main(int argc,char *argv[]) {
+	if (argc < 2) {
+	 	cerr << "Usage: " << argv[0] << " <json_path> <config_path>" << endl;
+		return 1;
+	}
+	string jsonpath=argv[1];
+	string configpath = argv[2];
+	HttpJson* server = new HttpJson(jsonpath, configpath);
     JsonApp config(configpath);
     JsonObject obj = config.getObject();
     map<string, SBU2WebService*>* jWebService = new map<string, SBU2WebService*>;
-    string end_url = "/content"; 
+    string end_url = obj["httpServer"]["endUrl"]; 
     int load = 1; 
-    int port = 8080; 
+    int port = 12345; 
     cout << "End URL: " << end_url << ", Load: " << load << ", Port: " << port << endl;
     (*jWebService)[end_url] = server;
     SBU2LoadBalancer* loadBalancer = new SBU2LoadBalancer(load, jWebService);
     SBU2HTTPServer* httpServer = new SBU2HTTPServer(port, loadBalancer);
     httpServer->run();
     while (1);
-     delete server;
-    delete loadBalancer;
-    delete httpServer;
     return 0;
 }
 
